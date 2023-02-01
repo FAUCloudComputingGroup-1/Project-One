67660c6719b4f4ed9371fa866bbd13d5bd97f67c
ChefOmarDee <74839667+ChefOmarDee@users.noreply.github.com>
2023-01-31 19:17:05 -0500

    Update

--- client/src/mods/FileUpload.js
+++ client/src/mods/FileUpload.js
@@ -1,31 +1,37 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import axios from 'axios'
 import download from 'downloadjs'
+import './FileUpload.css'
 let FileUpload=()=>{
     let[userFile,setUserFile]=useState()
     let[img, setImg]=useState()
     var[fName,setFName]=useState()
+    var count = useRef(0);
+
+    
     let ChangeHandler=(event)=>{
         setUserFile(URL.createObjectURL(event.target.files[0]))
         setImg(event.target.files[0])
         console.log(event.target.files[0])
         let len=event.target.files[0].name.length-4
-        setFName(event.target.files[0].name.slice(0, len) + "jpeg") ;
+        setFName(event.target.files[0].name.slice(0, len) + "jpeg");
+        count.current=1
     }
 
     let DownloadHandler = async () => {
         await console.log('bam')
-        const res = await fetch("http://localhost:8000/download");
+        const res = await fetch("http://100.26.106.118:8080/download");
         const blob = await res.blob();
         await console.log(blob)
         await download(blob,fName);
       }
+
     let SubmitHandler= async (event)=>{
         event.preventDefault()
         let data = await new FormData();
         await data.append('file', img)
         await console.log("wassup")
-        await axios.post("http://localhost:8000/upload", data, {})
+        await axios.post("http://100.26.106.118:8080/upload", data, {})
         .then(res => { 
         console.log(res.statusText)
         })
@@ -33,15 +39,31 @@ let FileUpload=()=>{
         await DownloadHandler()
         console.log(fName)
     }
-
+console.log(count.current)
+if(count.current===0){
     return(
         <React.Fragment>
             <form onSubmit={SubmitHandler}>
-                <input type="file" name ='file' accept='.jpeg' onChange={ChangeHandler}/>
-                <input type="submit"/>
+                <input className= 'pre_button1'type="file" title=" " name ='file' accept='.jpeg' onChange={ChangeHandler}/>
             </form>
-            <img src={userFile}/>
         </React.Fragment>
     )
+}
+else {
+  return(
+      <React.Fragment>
+          <form className='postform' onSubmit={SubmitHandler}>
+              <input className='button1' type="file" title=" " name ='file' accept='.jpeg' onChange={ChangeHandler}/>
+              <input className='button2' type="submit"/>
+          </form>
+          
+<div className="container">
+<div className="image">
+  <img src={userFile} className="img" alt=""/>
+</div>
+</div>
+      </React.Fragment>
+  )
+}
 }
 export default FileUpload
