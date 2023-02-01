d8819e1917d1b8f130bda0fbf38d3b2114510578
ChefOmarDee <74839667+ChefOmarDee@users.noreply.github.com>
2023-01-31 19:13:46 -0500

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import FileUpload from './mods/FileUpload';
+//comment test
 function App() {
   return (
   <React.Fragment>
-    <FileUpload/>
+  <span className='span3'>
+  <span className='span1'>
+  </span>
+    <div className="area" >
+            <ul className="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <div className='CENTRAL'>
+                    <FileUpload/>
+                    </div>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+    </div >
+    <span className='span2'>
+    </span>
+    </span>
   </React.Fragment>
   );
 }
